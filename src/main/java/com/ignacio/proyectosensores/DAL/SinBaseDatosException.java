8a5c6018318992ad3fdb91b1aa1dac569001e58b
ignacio <ignacio.ruz.donoso@gmail.com>
2015-12-01 11:52:07 -0300

    Pruebas con postgresql

---
+++ src/main/java/com/ignacio/proyectosensores/DAL/SinBaseDatosException.java
@@ -0,0 +1,13 @@
+package com.ignacio.proyectosensores.DAL;
+
+/**
+ *
+ * @author ignacio
+ */
+public class SinBaseDatosException extends Exception {
+
+	public SinBaseDatosException(Throwable t) {
+		super(t);
+	}
+
+}
--- src/main/java/com/ignacio/proyectosensores/Main.java
+++ src/main/java/com/ignacio/proyectosensores/Main.java
@@ -5,10 +5,30 @@
  */
 package com.ignacio.proyectosensores;
 
+import com.ignacio.proyectosensores.DAL.BD;
+import com.ignacio.proyectosensores.DAL.CodigoRepetidoException;
+import com.ignacio.proyectosensores.DAL.SinBaseDatosException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ignacio
  */
 public class Main {
-	
+	public static void main(String[] args) {
+		BD bd = null;
+		try {
+			bd = new BD();
+			bd.update("insert into lugar values (?,?)", 2, "Edificio");
+		} catch (SinBaseDatosException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (CodigoRepetidoException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}finally{
+			if (bd != null){
+				bd.close();
+			}
+		}
+	}
 }
