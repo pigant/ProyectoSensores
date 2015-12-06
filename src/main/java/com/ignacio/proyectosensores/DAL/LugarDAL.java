58440e55138d4bcd7006b289225eb5ccc9d77b50
Ignacio Ruz <pigant666@hotmail.com>
2015-12-06 18:16:27 -0300

    Se agrega una capa mas para operar los consultas a nivel de acceso de datos
    ObjectDAL, esta se usa actualmente en lugardal y maquinadal
    falta implementarla completamente

--- src/main/java/com/ignacio/proyectosensores/DAL/LugarDAL.java
+++ src/main/java/com/ignacio/proyectosensores/DAL/LugarDAL.java
@@ -4,8 +4,6 @@ import com.ignacio.proyectosensores.BLL.Lugar;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
