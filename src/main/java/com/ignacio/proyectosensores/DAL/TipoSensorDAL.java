0b1442c5e477afbf6de964ceab50c4f32f30ba5d
Ignacio Ruz <pigant666@hotmail.com>
2015-12-08 13:49:33 -0300

    Creacion de tipo de sensor

--- src/main/java/com/ignacio/proyectosensores/DAL/TipoSensorDAL.java
+++ src/main/java/com/ignacio/proyectosensores/DAL/TipoSensorDAL.java
@@ -3,6 +3,8 @@ package com.ignacio.proyectosensores.DAL;
 import com.ignacio.proyectosensores.BLL.TipoSensor;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,7 +17,7 @@ public class TipoSensorDAL {
 	public static TipoSensor find(int id) throws SinBaseDatosException {
 		TipoSensor ts = null;
 		Object[] find = ObjectDAL.find("select id_t_sensor, nombre "
-				+ "from t_sensor where id_t_sensor=?", id);
+			+ "from t_sensor where id_t_sensor=?", id);
 		if (find != null) {
 			ts = new TipoSensor((int) find[0], (String) find[1]);
 		}
@@ -24,26 +26,26 @@ public class TipoSensorDAL {
 
 	public static Integer guardar(String nombre) throws SinBaseDatosException, CodigoRepetidoException {
 		return ObjectDAL.guardar(
-				"insert into t_sensor (nombre) values (?)", nombre);
+			"insert into t_sensor (nombre) values (?)", nombre);
 	}
 
 	public static boolean actualizar(Integer id, String nombre) throws SinBaseDatosException {
 		return ObjectDAL.actualizar(
-				"update t_sensor set nombre=? where id_t_sensor=?",
-				nombre, id);
+			"update t_sensor set nombre=? where id_t_sensor=?",
+			nombre, id);
 	}
 
 	public static boolean delete(Integer id) throws SinBaseDatosException {
 		return ObjectDAL.delete(
-				"delete from t_sensor where id_t_sensor=?",
-				id);
+			"delete from t_sensor where id_t_sensor=?",
+			id);
 	}
 
 	public static TipoSensor findBySensor(Integer id) throws SinBaseDatosException {
 		TipoSensor ts = null;
 		String q = "select ts.id_t_sensor, ts.nombre from t_sensor as ts "
-				+ "join sensor as s on ts.id_t_sensor=s.id_t_sensor "
-				+ "where s.id_sensor=" + id;
+			+ "join sensor as s on ts.id_t_sensor=s.id_t_sensor "
+			+ "where s.id_sensor=" + id;
 		BD bd = new BD();
 		ResultSet r;
 		try {
@@ -58,6 +60,15 @@ public class TipoSensorDAL {
 			bd.close();
 		}
 		return ts;
+	}
+
+	public static List<TipoSensor> findAll() throws SinBaseDatosException {
+		ArrayList<Object[]> o = ObjectDAL.findAll("select id_t_sensor, nombre from t_sensor");
+		ArrayList<TipoSensor> t = new ArrayList(o.size());
+		for (Object[] ob : o) {
+			t.add(new TipoSensor((int) ob[0], (String) ob[1]));
+		}
+		return t;
 	}
 
 }
